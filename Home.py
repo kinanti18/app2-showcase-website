1cdfbfd0bd706955a5476545d3089db3e25fcb75
kinanti18 <anastasia.putri162001@gmail.com>
2023-11-24 14:20:29 +0700

    Day 23: Edit images

--- Home.py
+++ Home.py
@@ -34,12 +34,12 @@ with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
         st.write(row['description'])
-        st.write("images/" + row["image"])
+        st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
         st.write(row['description'])
-        st.write("images/" + row["image"])
+        st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
