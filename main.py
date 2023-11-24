a56e841dd607f66f7b7c0820ebb741903340c170
kinanti18 <anastasia.putri162001@gmail.com>
2023-11-24 11:21:12 +0700

    Day 22: Add app description, image and links

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas
 import streamlit as st
 
 st.set_page_config(layout="wide")
@@ -25,3 +26,20 @@ content2 = """
 Below you can find some of the apps I have built in Python. Feel free to contact me!
 """
 st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+df = pandas.read_csv("data.csv", sep=';')
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.write("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.write("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
